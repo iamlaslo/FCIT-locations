c5f6b3bd6c88950f972e42af87def93260536fb6
Anis <youhan90@gmail.com>
2021-03-04 02:44:42 +0200

    message delete, huge bug fixes

--- bot.js
+++ bot.js
@@ -1,59 +1,7 @@
 const { Telegraf, session, Scenes:{ WizardScene, Stage}, Markup} = require('telegraf');
-const TOKEN ='1699609256:AAG1Iq8avfrlULqnmqgEzyfQd8vZsBOvLZ4'
+const TOKEN = '1699609256:AAG1Iq8avfrlULqnmqgEzyfQd8vZsBOvLZ4'
 const bot = new Telegraf(TOKEN);
 const MongoClient = require('mongodb').MongoClient;
-bot.launch();
-
-bot.start(async (ctx) => {
-  console.log('Bot was used');
-
-  ctx.reply('Добро пожаловать!', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Кафе', callback_data: '/cafe', resize_keyboard: true },
-          {
-            text: 'Достопримечательность',
-            callback_data: '/location',
-            resize_keyboard: true,
-          },
-        ],[
-          {text: "Добавить место", callback_data: 'addplace'}
-        ]
-      ],
-    },
-  });
-  ctx.reply('Куда вы хотите сходить?', {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: 'Найти ближайшие',
-            request_location: true,
-            callback_data: '/try',
-            resize_keyboard: true,
-          },
-        ],
-      ],
-    },
-  });
-  ctx
-});
-
-addkeyboard = {reply_markup:{
-	inline_keyboard:[
-		[{text:"Кафе", callback_data: "cafe"}],
-		[{text:"Достопримечательность", callback_data: "place"}]
-	]
-}}
-
-cancelmore = {reply_markup:{
-  inline_keyboard:[
-    [{text:"Ещё", callback_data: "more"}]
-  ]
-}}
-
-let locs = [];
 
 const mongoClient = new MongoClient(
   'mongodb+srv://vlad:vlad123@cluster0.zfcnq.mongodb.net/test',
@@ -67,7 +15,6 @@ mongoClient.connect(async function (err, client) {
   collection.find().toArray(async function (err, results) {
     if (err) return console.log(err);
     res = results;
-    res;
     if (res.length > 0) {
       for (doc of res) {
         locs.push(await doc);
@@ -76,7 +23,62 @@ mongoClient.connect(async function (err, client) {
   });
 });
 
+canceladd = {reply_markup: {
+  inline_keyboard: [
+    [{text:"Отмена", callback_data: "canceladd"}]
+  ]
+}}
+
+addkeyboard = {reply_markup:{
+  inline_keyboard:[
+  [{text:"Кафе", callback_data: "cafe"}],
+  [{text:"Достопримечательность", callback_data: "place"}]
+  ]
+}}
+
+more = {reply_markup:{
+  inline_keyboard:[
+  [{text:"Ещё", callback_data: "more"}, {text: "Отмена", callback_data: "canceladd"}]
+  ]
+}}
+
+locationkeyboard = {reply_markup:{
+  resize_keyboard: true,
+  one_time_keyboard: true,
+  keyboard: [ [{text: 'Поделиться локацией', request_location: true}] ]
+}}
+
+
+mainkeyboard = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        { text: 'Кафе', callback_data: '/cafe', resize_keyboard: true },
+        {
+          text: 'Достопримечательность',
+          callback_data: '/place',
+          resize_keyboard: true,
+        },
+      ],
+      [{text: "Добавить место", callback_data: 'addplace'}],
+      [{text: 'Найти ближайшие', callback_data: 'showlocation'}]
+    ]
+  }
+}
+
+
+bot.command('start', async (ctx) => {
+for(let j=0;j<=50;j++) {
+  ctx.deleteMessage(ctx.message.message_id-j);
+}
+  console.log('Bot was used');
+  ctx.reply('Добро пожаловать!', mainkeyboard)
+});
+
+let locs = [];
+
 bot.on('location', (ctx) => {
+  ctx.deleteMessage(ctx.message.message_id-1)
   let currentLat = ctx.update.message.location.latitude;
   let currentLon = ctx.update.message.location.longitude;
 
@@ -146,13 +148,20 @@ bot.on('location', (ctx) => {
     setTimeout(() => {
       ctx.tg.sendLocation(ctx.chat.id, locs[2].x, locs[2].y);
     }, 2000);
+    setTimeout(() => {
+      ctx.reply('Что хотите сделать?', mainkeyboard)
+    }, 3000);
   }, 6000);
 });
 
+bot.action('showlocation', ctx =>{
+  ctx.deleteMessage()
+  ctx.reply('Пожалуйста покажите, где Вы', locationkeyboard)
+})
 
 i = 0
-const show_cafe = new WizardScene(
-  'show-cafe',
+const show_place = new WizardScene(
+  'show-place',
   ctx =>{
     ctx.replyWithPhoto({ url: locs[i].image });
     setTimeout(() => {
@@ -164,25 +173,20 @@ const show_cafe = new WizardScene(
     setTimeout(() => {
       if(i===res.length-1) i=0
       else i++
-      ctx.reply('More?', cancelmore)
+      ctx.reply('Показать ещё?', more)
     },3000);
     return ctx.wizard.next()
   },
   ctx =>{
     ctx.deleteMessage()
     ctx.scene.leave()
-    if(callback_data='more') ctx.scene.enter('show-cafe')
+    if(callback_data='more') ctx.scene.enter('show-place')
   }
 )
 
 const add_data = new WizardScene(
 	'add-data',
 	ctx => {
-		canceladd = {reply_markup: {
-			inline_keyboard: [
-				[{text:"Отмена", callback_data: "canceladd"}]
-			]
-		}}
 		ctx.reply('Введите название места: ',canceladd);
 		return ctx.wizard.next();
 	},
@@ -231,22 +235,24 @@ const add_data = new WizardScene(
 	}
 )
 
-const stage = new Stage([add_data, show_cafe])
+const stage = new Stage([add_data, show_place])
 
 stage.action('canceladd', ctx => {
   ctx.deleteMessage()
-	ctx.reply('Отменено')
 	ctx.scene.leave()
-})
+  ctx.reply('Что хотите сделать?', mainkeyboard)
+});
 
 bot.use(session())
 bot.use(stage.middleware())
 
-bot.action('/cafe', (ctx) => {
-  ctx.scene.enter('show-cafe')
+bot.action('/place', (ctx) => {
+  ctx.deleteMessage()
+  ctx.scene.enter('show-place')
 });
 
 bot.action('addplace', (ctx) =>{
+  ctx.deleteMessage()
   ctx.reply('Что за место вы хотите добавить?', addkeyboard)
 })
 
@@ -268,9 +274,11 @@ bot.action('acceptdb', ctx =>{
 	    const db = client.db("FCIT-locations")
 	    const collection = db.collection("offerlocations")
 	    if(err) return console.log(err)
-		collection.insertOne(place, function(err, result){
+		  collection.insertOne(place, function(err, result){
 	        if(err) return console.log(err)
-	        ctx.editMessageText("Успешно добавлено в базу данных\nВаша анкета будет рассмотрена администраторами")
+	        ctx.editMessageText("Успешно добавлено в базу данных\nВаша анкета будет рассмотрена администраторами", mainkeyboard)
     	})
 	})
 })
+
+bot.launch();
