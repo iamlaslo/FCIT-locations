87764d6d768463a79442343638f2201433e6a291
Anis <youhan90@gmail.com>
2021-03-07 21:07:23 +0200

    url to photo, coordinates to location

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Telegraf, session, Scenes:{ WizardScene, Stage}, Markup} = require('telegraf');
-const TOKEN = '1699609256:AAG1Iq8avfrlULqnmqgEzyfQd8vZsBOvLZ4'
+const TOKEN ='1663994003:AAF02oK7kVii4QSXxJ2edLdKuNUkyT47zq4' //main '1699609256:AAG1Iq8avfrlULqnmqgEzyfQd8vZsBOvLZ4'
 const bot = new Telegraf(TOKEN);
 const MongoClient = require('mongodb').MongoClient;
 
@@ -24,9 +24,9 @@ mongoClient.connect(async function (err, client) {
 });
 
 canceladd = {reply_markup: {
-  inline_keyboard: [
-    [{text:"Отмена", callback_data: "canceladd"}]
-  ]
+  resize_keyboard: true,
+  one_time_keyboard: true,
+  keyboard: [ [{text:"Отмена"}] ]
 }}
 
 addkeyboard = {reply_markup:{
@@ -38,16 +38,17 @@ addkeyboard = {reply_markup:{
 
 more = {reply_markup:{
   inline_keyboard:[
-  [{text:"Ещё", callback_data: "more"}, {text: "Отмена", callback_data: "canceladd"}]
+  [{text:"Ещё", callback_data: "more"}, {text: "Отмена", callback_data: "canceldb"}]
   ]
 }}
 
 locationkeyboard = {reply_markup:{
   resize_keyboard: true,
   one_time_keyboard: true,
-  keyboard: [ [{text: 'Поделиться локацией', request_location: true}] ]
+  keyboard: [ 
+    [{text: 'Поделиться локацией', request_location: true}]
+  ]
 }}
-
 
 mainkeyboard = {
   reply_markup: {
@@ -66,104 +67,11 @@ mainkeyboard = {
   }
 }
 
-
-bot.command('start', async (ctx) => {
-for(let j=0;j<=50;j++) {
-  ctx.deleteMessage(ctx.message.message_id-j);
-}
-  console.log('Bot was used');
-  ctx.reply('Добро пожаловать!', mainkeyboard)
-});
-
-let locs = [];
-
-bot.on('location', (ctx) => {
-  ctx.deleteMessage(ctx.message.message_id-1)
-  let currentLat = ctx.update.message.location.latitude;
-  let currentLon = ctx.update.message.location.longitude;
-
-  findNear = (lat1, lon1, lat2, lon2, place) => {
-    const R = 6371e3; // metres
-    const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
-    const φ2 = (lat2 * Math.PI) / 180;
-    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
-    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
-
-    const a =
-      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    const d = R * c;
-    place.distance = d;
-  };
-  for (place of locs) {
-    findNear(currentLat, currentLon, place.x, place.y, place);
-    console.log(place.distance);
-  }
-
-  locs.sort((a, b) =>
-    a.distance > b.distance
-      ? 1
-      : a.distance === b.distance
-      ? a.size > b.size
-        ? 1
-        : -1
-      : -1
-  );
-
-  console.log(locs);
-
-  setTimeout(() => {
-    ctx.replyWithPhoto({ url: locs[0].image });
-
-    setTimeout(() => {
-      ctx.reply(locs[0].name + ' - ' + locs[0].desc);
-    }, 1000);
-
-    setTimeout(() => {
-      ctx.tg.sendLocation(ctx.chat.id, locs[0].x, locs[0].y);
-    }, 2000);
-  }, 1000);
-
-  setTimeout(() => {
-    ctx.replyWithPhoto({ url: locs[1].image });
-
-    setTimeout(() => {
-      ctx.reply(locs[1].name + ' - ' + locs[1].desc);
-    }, 1000);
-
-    setTimeout(() => {
-      ctx.tg.sendLocation(ctx.chat.id, locs[1].x, locs[1].y);
-    }, 2000);
-  }, 3000);
-
-  setTimeout(() => {
-    ctx.replyWithPhoto({ url: locs[2].image });
-
-    setTimeout(() => {
-      ctx.reply(locs[2].name + ' - ' + locs[2].desc);
-    }, 1000);
-
-    setTimeout(() => {
-      ctx.tg.sendLocation(ctx.chat.id, locs[2].x, locs[2].y);
-    }, 2000);
-    setTimeout(() => {
-      ctx.reply('Что хотите сделать?', mainkeyboard)
-    }, 3000);
-  }, 6000);
-});
-
-bot.action('showlocation', ctx =>{
-  ctx.deleteMessage()
-  ctx.reply('Пожалуйста покажите, где Вы', locationkeyboard)
-})
-
 i = 0
 const show_place = new WizardScene(
   'show-place',
   ctx =>{
-    ctx.replyWithPhoto({ url: locs[i].image });
+    ctx.replyWithPhoto( locs[i].image );
     setTimeout(() => {
       ctx.reply(locs[i].name + ' - ' + locs[i].desc);
     }, 1000);
@@ -184,6 +92,87 @@ const show_place = new WizardScene(
   }
 )
 
+const findlclose = new WizardScene(
+  'find-close',
+  ctx=>{
+    ctx.deleteMessage(ctx.message.message_id-1)
+    let currentLat = ctx.update.message.location.latitude;
+    let currentLon = ctx.update.message.location.longitude;
+  
+    findNear = (lat1, lon1, lat2, lon2, place) => {
+      const R = 6371e3; // metres
+      const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+      const φ2 = (lat2 * Math.PI) / 180;
+      const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+      const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+  
+      const a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+      const d = R * c;
+      place.distance = d;
+    };
+    for (place of locs) {
+      findNear(currentLat, currentLon, place.x, place.y, place);
+      console.log(place.distance);
+    }
+  
+    locs.sort((a, b) =>
+      a.distance > b.distance
+        ? 1
+        : a.distance === b.distance
+        ? a.size > b.size
+          ? 1
+          : -1
+        : -1
+    );
+  
+    console.log(locs);
+  
+    setTimeout(() => {
+      ctx.replyWithPhoto({ url: locs[0].image });
+  
+      setTimeout(() => {
+        ctx.reply(locs[0].name + ' - ' + locs[0].desc);
+      }, 1000);
+  
+      setTimeout(() => {
+        ctx.tg.sendLocation(ctx.chat.id, locs[0].x, locs[0].y);
+      }, 2000);
+    }, 1000);
+  
+    setTimeout(() => {
+      ctx.replyWithPhoto({ url: locs[1].image });
+  
+      setTimeout(() => {
+        ctx.reply(locs[1].name + ' - ' + locs[1].desc);
+      }, 1000);
+  
+      setTimeout(() => {
+        ctx.tg.sendLocation(ctx.chat.id, locs[1].x, locs[1].y);
+      }, 2000);
+    }, 3000);
+  
+    setTimeout(() => {
+      ctx.replyWithPhoto({ url: locs[2].image });
+  
+      setTimeout(() => {
+        ctx.reply(locs[2].name + ' - ' + locs[2].desc);
+      }, 1000);
+  
+      setTimeout(() => {
+        ctx.tg.sendLocation(ctx.chat.id, locs[2].x, locs[2].y);
+      }, 2000);
+      setTimeout(() => {
+        ctx.reply('Что хотите сделать?', mainkeyboard)
+      }, 3000);
+    }, 6000);
+    ctx.scene.leave()
+  }
+)
+
 const add_data = new WizardScene(
 	'add-data',
 	ctx => {
@@ -194,57 +183,88 @@ const add_data = new WizardScene(
     ctx.deleteMessage()
     ctx.deleteMessage(ctx.message.message_id-1)
 		name = ctx.message.text;
-		ctx.reply('Введите короткое описание места: ',canceladd);
+		ctx.reply('Введите короткое описание места: ', canceladd);
 		return ctx.wizard.next();
 	},
 	ctx =>{
+    t=0
     ctx.deleteMessage()
     ctx.deleteMessage(ctx.message.message_id-1)
     desc = ctx.message.text;
-		ctx.reply('Отправьте ссылку на фото места: ',canceladd);
+		ctx.reply('Отправьте фото места: ', canceladd);
 		return ctx.wizard.next();
 	},
 	ctx =>{
+    if (ctx.message.photo || t==1){
+    t=0
   	ctx.deleteMessage()
     ctx.deleteMessage(ctx.message.message_id-1)
-    image = ctx.message.text;
-		ctx.reply('Отправьте координату x места: ',canceladd);
+    if (ctx.message.photo)image = ctx.message.photo[ctx.message.photo.length-1].file_id
+		ctx.reply('Отправьте геолокацию места: ', canceladd)
 		return ctx.wizard.next();
+    } else {
+      ctx.wizard.back()
+      return ctx.wizard.steps[ctx.wizard.cursor](ctx)
+    }
 	},
 	ctx =>{
-    ctx.deleteMessage()
-    ctx.deleteMessage(ctx.message.message_id-1)
-    x = ctx.message.text;
-		ctx.reply('Отправьте координату y места: ',canceladd);
-		return ctx.wizard.next();
-	},
-	ctx =>{
-    ctx.deleteMessage()
-    ctx.deleteMessage(ctx.message.message_id-1)
-    y = ctx.message.text;
-		ctx.reply('Название: ' + name +'\nОписание: '+desc+'\nФото: '+image+'\nКоордината x: '+x+'\nКоордината y: '+y+'\nВсё верно?',
-		{
-			reply_markup: {
-				inline_keyboard: [
-					[{text:"Подтвердить", callback_data: "acceptdb"}],
-					[{text:"Отмена", callback_data: "canceldb"}]
-				]
-			}
-		});
-		return ctx.scene.leave();
-	}
+      if(ctx.message.location){
+      ctx.deleteMessage()
+      ctx.deleteMessage(ctx.message.message_id-1)
+      x = ctx.message.location.latitude
+      y = ctx.message.location.longitude
+      ctx.replyWithPhoto(image)
+      setTimeout(() => {
+      ctx.reply('Название: ' + name +'\nОписание: '+desc+'\nКоордината x: '+x+'\nКоордината y: '+y+'\nВсё верно?',
+      {
+        reply_markup: {
+            resize_keyboard: true,
+            one_time_keyboard: true,
+          keyboard: [
+            [{text:"Подтвердить"}],
+            [{text:"Отмена"}]
+          ]
+        }
+      });
+      }, 1000);
+      dbaccepted=1
+      return ctx.scene.leave();
+    }else {
+      t=1
+      ctx.wizard.back()
+      return ctx.wizard.steps[ctx.wizard.cursor](ctx)
+    }
+  }
 )
 
-const stage = new Stage([add_data, show_place])
+const stage = new Stage([add_data, show_place, findlclose])
 
-stage.action('canceladd', ctx => {
+stage.hears('Отмена', ctx => {
   ctx.deleteMessage()
+  ctx.deleteMessage(ctx.message.message_id-1)
 	ctx.scene.leave()
   ctx.reply('Что хотите сделать?', mainkeyboard)
 });
 
 bot.use(session())
 bot.use(stage.middleware())
+
+
+bot.command('start', async (ctx) => {
+  for(let j=0;j<=50;j++) {
+    ctx.deleteMessage(ctx.message.message_id-j);
+  }
+    console.log('Bot was used');
+    ctx.reply('Добро пожаловать!', mainkeyboard)
+});
+  
+let locs = [];
+
+bot.action('showlocation', ctx =>{
+    ctx.deleteMessage()
+    ctx.reply('Пожалуйста покажите, где Вы', locationkeyboard)
+    ctx.scene.enter('find-close')
+})
 
 bot.action('/place', (ctx) => {
   ctx.deleteMessage()
@@ -268,17 +288,23 @@ bot.action('place', ctx =>{
 	ctx.scene.enter('add-data')
 })
 
-bot.action('acceptdb', ctx =>{
-	let place = {name: name, desc: desc, image: image, x: x, y: y, type: type}
+bot.hears('Подтвердить', ctx =>{
+  if(dbaccepted!==1) return
+	else {
+  let place = {name: name, desc: desc, image: image, x: x, y: y, type: type}
 	mongoClient.connect(function(err, client){
 	    const db = client.db("FCIT-locations")
 	    const collection = db.collection("offerlocations")
 	    if(err) return console.log(err)
 		  collection.insertOne(place, function(err, result){
 	        if(err) return console.log(err)
-	        ctx.editMessageText("Успешно добавлено в базу данных\nВаша анкета будет рассмотрена администраторами", mainkeyboard)
+	        ctx.reply("Успешно добавлено в базу данных\nВаша анкета будет рассмотрена администраторами", mainkeyboard)
     	})
 	})
+  }
 })
 
+bot.on('message', ctx =>{
+  ctx.deleteMessage()
+})
 bot.launch();
